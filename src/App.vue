15eb2f13e479e6b2f06eb388f70ece05d6e6d164
Yaisa Arteaga <yaisamichellea@gmail.com>
2023-01-03 14:59:47 -0500

    basic structure sign up view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SingUp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SingUp from './components/SignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SingUp
+}
 }
 </script>
 
@@ -22,5 +21,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: #121212;
 }
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,49 @@
+<template>
+    <img class="logo" src="../assets/Logo.png"/>
+    <h1 class="title">Registro</h1>
+    <div class="register">
+        <input type="text" placeholder="Ingresa tu Nombre" />
+        <input type="text" placeholder="Ingresa tu Email" />
+        <input type="text" placeholder="Ingresa tu Contraseña" />
+        <button>Registrate</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp',
+}
+</script>
+
+<style>
+.logo {
+    width: 250px;
+}
+.title {
+    color: rgb(247, 243, 243);
+}
+.register input {
+    width: 300px;
+    height: 35px;
+    padding-left: 15px;
+    display: block;
+    margin-bottom: 20px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid white;
+    background: none;
+    border-radius: 10px;
+}
+.register button {
+    width: 320px;
+    height: 35px;
+    border: 1px solid white;
+    background-color: #121212;
+    color: white;
+    border-radius: 10px;
+}
+.register button:hover {
+    background-color: white;
+    color: #121212;
+}
+</style>
